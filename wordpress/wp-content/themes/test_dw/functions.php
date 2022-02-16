c6db3aa9ca3b20d9a44394703017dafe45e124e7
theoleonet <theo.leonet@student.hepl.be>
2022-02-16 12:07:05 +0100

    :recycle: added custom post type

--- wordpress/wp-content/themes/test_dw/functions.php
+++ wordpress/wp-content/themes/test_dw/functions.php
@@ -5,3 +5,16 @@ add_filter('use_block_editor_for_post', '__return_false');
 
 // Activer les images sur les articles
 add_theme_support('post-thumbnails');
+
+//Enregistrer un seul custom post type pour nos voyages
+register_post_type('trip', [
+    'label' => 'Voyages',
+    'labels' => [
+      'name' => 'Voyages',
+      'singular_name' => 'Voyage',
+    ],
+    'description' => 'Tous les articles qui décrivent un voyage',
+    'public' => true,
+    'menu_position' => 4,
+    'menu_icon' => 'dashicons-palmtree',
+]);
