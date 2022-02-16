8ee9e1752d0af67122dcdedc8b9a7941c4065492
theoleonet <theo.leonet@student.hepl.be>
2022-02-16 11:53:11 +0100

    :recycle: added dynamic articles

--- wordpress/wp-content/themes/test_dw/functions.php
+++ wordpress/wp-content/themes/test_dw/functions.php
@@ -1,1 +1,7 @@
 <?php
+
+// Désactiver l'éditeur "Gutenberg" de Wordpress
+add_filter('use_block_editor_for_post', '__return_false');
+
+// Activer les images sur les articles
+add_theme_support('post-thumbnails');
--- wordpress/wp-content/themes/test_dw/index.php
+++ wordpress/wp-content/themes/test_dw/index.php
@@ -1,5 +1,42 @@
 <?php get_header(); ?>
 
-<!-- le contenu de mon template -->
+<main class="layout">
+    <section class="layout_latest latest">
+        <h2 class="latest_title">
+            Mes derniers articles
+        </h2>
+        <div class="latest_container">
+            <?php if(have_posts()): while(have_posts()): the_post();?>
+                <!-- On est dans la boucle -->
+                <article class="post">
+                    <a href="<?= get_the_permalink(); ?>" class="post__link">
+                        Lire l'article "<?= get_the_title(); ?>"
+                    </a>
+                    <div class="post__card">
+                        <header class="post__head">
+                            <h3 class="post__title">
+                                <?= get_the_title(); ?>
+                            </h3>
+                            <p class="post__meta">
+                                Publié par <?= get_the_author(); ?> le <time class="post__date" datetime="<?= get_the_date('c'); ?>"><?= get_the_date(); ?></time>
+                            </p>
+                        </header>
+                        <figure class="post__fig">
+                            <!--<img src="#" alt="TODO" class="post__thumb">-->
+                            <?= get_the_post_thumbnail(null, 'medium_large', ['class' => 'post__thumb']); ?>
+                        </figure>
+                        <div class="post__excerpt">
+                            <p>
+                                <?= get_the_excerpt(); ?>
+                            </p>
+                        </div>
+                    </div>
+                </article>
+            <?php endwhile; else:?>
+                <!-- Il n'y a rien à afficher -->
+            <?php endif; ?>
+        </div>
+    </section>
+</main>
 
 <?php get_footer(); ?>
