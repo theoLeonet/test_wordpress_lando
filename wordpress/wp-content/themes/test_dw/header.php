2584e8ee94a40ef1f16b5e0b869a27f67c2a101d
theoleonet <theo.leonet@student.hepl.be>
2022-02-16 09:10:45 +0100

    :recycle: added content to footer and header

--- wordpress/wp-content/themes/test_dw/header.php
+++ wordpress/wp-content/themes/test_dw/header.php
@@ -1,1 +1,29 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <header class="header">
+        <h1 class="header__title">
+            <?= get_bloginfo('name'); ?>
+        </h1>
+        <p class="header__tagline">
+            <?= get_bloginfo('description'); ?>
+        </p>
+
+        <nav class="header__nav nav">
+            <h2 class="nav__title">
+                Navigation principale
+            </h2>
+            <p class="nav__placeholder">
+                TODO...
+            </p>
+        </nav>
+    </header>
+</body>
+</html>
