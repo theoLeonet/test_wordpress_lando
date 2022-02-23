b740a58fa02d5d6e53642e43fda85979f34c83f5
theoleonet <theo.leonet@student.hepl.be>
2022-02-23 15:05:52 +0100

    :recycle: Added ACF fields for trip post type

--- wp-content/themes/test_dw/index.php
+++ wp-content/themes/test_dw/index.php
@@ -47,7 +47,7 @@
                     <header class="trip__head">
                         <h3 class="trip__title"><?= get_the_title(); ?></h3>
                         <p class="trip__date">
-                            <time class="trip__time" datetime=""></time>
+                            <time class="trip__time" datetime="<?= date('c', strtotime(get_field('departure_date', false, false))) ?>"><?= ucfirst(date_i18n('F, Y', strtotime(get_field('departure_date', false, false))))  ?></time>
                         </p>
                     </header>
                     <figure class="trip__fig">
