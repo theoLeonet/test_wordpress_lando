d35e3c778fc1a91074c4a6262bd12231219e9908
theoleonet <theo.leonet@student.hepl.be>
2022-03-16 08:53:39 +0100

    :bento: Updated PrimaryMenuItem.php, functions.php, header.php
    Fixed not working code.

--- wp-content/themes/test_dw/Menus/PrimaryMenuItem.php
+++ wp-content/themes/test_dw/Menus/PrimaryMenuItem.php
@@ -1,14 +1,17 @@
 <?php
 
-class PrimaryMenuItem{
 
+class PrimaryMenuItem
+{
     protected $post;
+
     public $url;
     public $title;
     public $label;
     public $subitems = [];
 
-    function __construct($post){
+    function __construct($post)
+    {
         $this->post = $post;
 
         $this->url = $post->url;
@@ -16,50 +19,60 @@ class PrimaryMenuItem{
         $this->title = $post->attr_title;
     }
 
-    public function hasSubItems(){
-        //Regarder s'il y a des éléments dans $this->subitems. Si c'est le cas, cet $item possède effectivement un sous menu
-        return ! empty($this->subitems);
+    public function hasSubItems()
+    {
+        // Regarder s'il y a des éléments dans $this->subitems. Si c'est le cas,
+        // cet $item possède effectivement un sous-menu.
+        return !empty($this->subitems);
     }
 
-    public function isSubItem(){
-        //Regarder si cet $item possède un identifiant de post parent. Si c'est le cas,
+    public function isSubItem()
+    {
+        // Regarder si cet $item possède un identifiant de post parent.
+        // Si c'est le cas, cet $item est un sous-item.
         return boolval($this->getParentId());
     }
 
-    public function isParentFor(PrimaryMenuItem $instance){
-        //Regarder si l'élément fournit possède une référence de parent qui correspond à l'idientifiant de cet $item
-        return ($this->post->ID === $instance->getParentId());
+    public function isParentFor(PrimaryMenuItem $instance)
+    {
+        // Regarder si l'élément fourni possède une référence de "parent"
+        // qui correspond à l'identifiant de cet $item.
+        return ($this->post->ID == $instance->getParentId());
     }
 
-    public function getParentId(){
-        //Retourner l'identifiant (référence) de l'item parent
+    public function getParentId()
+    {
+        // Retourner l'identifiant (référence) de l'item parent.
         return $this->post->menu_item_parent;
     }
 
-    public function addSubItem(PrimaryMenuItem $instance){
-        //Ajouter l'instance de ...
+    public function addSubItem(PrimaryMenuItem $instance)
+    {
+        // Ajouter l'instance "enfant" fournie au tableau de sous-éléments.
         $this->subitems[] = $instance;
     }
 
-    public function getBemClasses($base){
-        $icon = get_field('icon', $item);
-        $container_classes = [];
+    public function getBemClasses($base)
+    {
+        $modifiers = [];
 
-        if($icon){
-            $container_classes[] = $icon;
+        $icon = get_field('icon', $this->post);
+
+        if ($icon) {
+            $modifiers[] = $icon;
         }
 
-        if($item->current){
-            $container_classes[] = 'current';
+        if ($this->post->object_id == get_queried_object_id()) {
+            $modifiers[] = 'current';
         }
 
-        if(in_array('menu-item-type-custom', $item->classes)){
-            $container_classes[] = 'custom';
+        if (in_array('menu-item-type-custom', $this->post->classes)) {
+            $modifiers[] = 'custom';
         }
 
         $value = $base;
 
-        foreach ($modifiers as $modifier){
+        foreach ($modifiers as $modifier) {
             $value .= ' ' . $base . '--' . $modifier;
         }
 
--- wp-content/themes/test_dw/functions.php
+++ wp-content/themes/test_dw/functions.php
@@ -1,6 +1,6 @@
 <?php
 
-//Charger les fichiers nécessaires
+// Charger les fichiers nécessaires
 require_once(__DIR__ . '/Menus/PrimaryMenuWalker.php');
 require_once(__DIR__ . '/Menus/PrimaryMenuItem.php');
 
@@ -10,30 +10,25 @@ add_filter('use_block_editor_for_post', '__return_false');
 // Activer les images sur les articles
 add_theme_support('post-thumbnails');
 
-//Enregistrer un seul custom post type pour nos voyages
+// Enregistrer un seul custom post-type pour "nos voyages"
 register_post_type('trip', [
     'label' => 'Voyages',
     'labels' => [
-      'name' => 'Voyages',
-      'singular_name' => 'Voyage',
+        'name' => 'Voyages',
+        'singular_name' => 'Voyage',
     ],
     'description' => 'Tous les articles qui décrivent un voyage',
     'public' => true,
-    'menu_position' => 4,
+    'menu_position' => 5,
     'menu_icon' => 'dashicons-palmtree',
-    'supports' => [
-        'thumbnail',
-        'title',
-        'editor',
-    ],
-    'rewrite' => [
-        'slug' => 'voyages'
-    ]
+    'supports' => ['title','editor','thumbnail'],
+    'rewrite' => ['slug' => 'voyages'],
 ]);
 
-//Récupérer les trips via une requête wordpress.
-function testdw_get_trips($count = 20){
-    // 1. On instancie l'objet WP_Query
+// Récupérer les trips via une requête Wordpress
+function testdw_get_trips($count = 20)
+{
+    // 1. on instancie l'objet WP_Query
     $trips = new WP_Query([
         'post_type' => 'trip',
         'orderby' => 'date',
@@ -41,50 +36,51 @@ function testdw_get_trips($count = 20){
         'posts_per_page' => $count,
     ]);
 
-    // 2. On retourne l'objet WP_Query
+    // 2. on retourne l'objet WP_Query
     return $trips;
 }
 
-//Enregistrer les zones de menus
-register_nav_menu('primary', 'Navigation principale (Haut de page)');
+// Enregistrer les zones de menus
+
+register_nav_menu('primary', 'Navigation principale (haut de page)');
 register_nav_menu('footer', 'Navigation de pied de page');
 
-//Fonction pour récupérer les éléments d'un menu sous forme d'un tableau d'objets
-function testdw_get_menu_items($location){
+// Fonction pour récupérer les éléments d'un menu sous forme d'un tableau d'objets
+
+function testdw_get_menu_items($location)
+{
     $items = [];
 
-    //Récupérer le menu WordPress pour $location
+    // Récupérer le menu Wordpress pour $location
     $locations = get_nav_menu_locations();
 
-    if($locations[$location] ?? false){
+    if($locations[$location] ?? false) {
         $menu = $locations[$location];
 
-        //Récupérer tous les éléments du menu récupéré
+        // Récupérer tous les éléments du menu récupéré
         $posts = wp_get_nav_menu_items($menu);
 
-        //Formater chaque élément dans une instance de classe personnalisée
-    }
+        // Formater chaque élément dans une instance de classe personnalisée
+        // Boucler sur chaque $post
+        foreach($posts as $post) {
+            // Transformer le WP_Post en une instance de notre classe personnalisée
+            $item = new PrimaryMenuItem($post);
 
-    //Retourner un tableau d'éléments du menu formaté
-    //Boucler sur chaque $post
-    foreach ($posts as $post){
-        //Transformer le WP_Post en une instance de notre classe personnalisée
-        $item = new PrimaryMenuItem($post);
-
-        //Ajouter cette instance à $items ou à l'item parent si sous-menu
-        if($item->isSubItem()){
-            //Ajouter $item comme "enfant" de l'item parent
-            foreach ($items as $parent){
-                if($parent->isParentFor($item)){
-                    $parent->addSubitem($item);
+            // Ajouter cette instance à $items ou à l'item parent si sous-menu
+            if($item->isSubItem()) {
+                // Ajouter $item comme "enfant" de l'item parent.
+                foreach($items as $parent) {
+                    if($parent->isParentFor($item)) {
+                        $parent->addSubItem($item);
+                    }
                 }
+            } else {
+                // Ajouter au tableau d'éléments de niveau 0.
+                $items[] = $item;
             }
-        }else{
-            //Ajouter au tableau d'éléments de niveau 0
-            $items[] = $item;
-
         }
     }
 
+    // Retourner un tableau d'éléments du menu formatés
     return $items;
 }
